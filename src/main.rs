6341ebcb54149b0abaa1b20411103752e1f7a059
Aleks Kudashkin <jedi5kittens@gmail.com>
2018-08-03 11:19:59 -0400

    Input of numbeb by user

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use std::io;
+
+fn main() {
+    println!("Угадайте число!");
+
+    println!("Пожалуйста, введите предположение.");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Не удалось прочитать строку");
+
+    println!("Ваша попытка: {}", guess);
+}
